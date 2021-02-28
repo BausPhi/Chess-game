3c4ce19019462baff4c920f7c34f0746d0c93aad
BausPhi <philipp.baus.studium@googlemail.com>
2021-02-28 18:29:19 +0100

    Test runner now also runs the third test file

--- tests/runner.py
+++ tests/runner.py
@@ -1,9 +1,11 @@
 if __name__ == "__main__":
     import unittest
     from tests import test_figures_update_possible_moves
+    from tests import test_figures_get_possible_moves
     from tests import test_game_logic
 
-    suite_possible_moves = unittest.TestLoader().loadTestsFromModule(test_figures_update_possible_moves)
+    suite_update_possible_moves = unittest.TestLoader().loadTestsFromModule(test_figures_update_possible_moves)
     suite_game_logic = unittest.TestLoader().loadTestsFromModule(test_game_logic)
-    suite = unittest.TestSuite([suite_possible_moves, suite_game_logic])
+    suite_get_possible_moves = unittest.TestLoader().loadTestsFromModule(test_figures_get_possible_moves)
+    suite = unittest.TestSuite([suite_update_possible_moves, suite_game_logic, suite_get_possible_moves])
     unittest.TextTestRunner(verbosity=2).run(suite)
